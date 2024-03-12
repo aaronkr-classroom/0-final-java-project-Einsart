cc91c1c1364af06df27245a8903788e4295d7c58
Einswart <162276768+Einsart@users.noreply.github.com>
2024-03-12 16:51:54 +0900

    2주차

--- src/welcome.java
+++ src/welcome.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Type your name: ");
+		String name = input.next();
+		
+		System.out.println("연락처를 입력하세요: ");
+		int phone = input.nextint();
+		System.out.println("Your age: " + age);
+		
+		
+		
 		String greeting = "welcome to shopping Mall!";
 		String tagline = "Welcome to book Market!";
 		System.out.println("************************************************");
@@ -15,6 +26,10 @@ public class welcome {
 		System.out.println("3. 장바구니 비우기\t7. 장바구니에 항목 삭제하기");
 		System.out.println("4. 영수증 표시하기\t8. 종료");
 		System.out.println("************************************************");
+		
+		System.out.println("메뉴 번호를 선택하세요: ");
+		int choice = input.nextint();
+		System.out.println("choice + "번을 선택했습니다.);
 	}
 
 }
