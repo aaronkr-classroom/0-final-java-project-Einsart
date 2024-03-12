3f296283a454d95368c6f0feec6c7f05f3f32faf
Einswart <162276768+Einsart@users.noreply.github.com>
2024-03-12 14:17:20 +0900

    level 1

---
+++ src/bookmarket.java
@@ -0,0 +1,11 @@
+
+public class bookmarket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Book Martket");
+		System.out.println("Welcome to shopping mall");
+
+	}
+
+}
