6f9887ea47ab90fa9c08fd613cc7aca3b776e25e
Einswart <162276768+Einsart@users.noreply.github.com>
2024-03-19 16:24:07 +0900

    Update welcome.java

--- src/welcome.java
+++ src/welcome.java
@@ -5,31 +5,70 @@ public class welcome {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Type your name: ");
+		System.out.print("Type your name: ");
 		String name = input.next();
 		
-		System.out.println("연락처를 입력하세요: ");
-		int phone = input.nextint();
-		System.out.println("Your age: " + age);
+		System.out.print("연락처를 입력하세요: ");
+		int phone = input.nextInt(); // 숫자만
+		System.out.println("Your phone: " + phone);
 		
 		
 		
 		String greeting = "welcome to shopping Mall!";
 		String tagline = "Welcome to book Market!";
-		System.out.println("************************************************");
-		System.out.println("\t" + greeting);
-		System.out.println("\t" + tagline);
-		System.out.println("************************************************");
 		
-		System.out.println("1. 고객 정보 확인하기 \t5. 바구니에 항목 추가하기");
-		System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니에 항목 수량 줄이기");
-		System.out.println("3. 장바구니 비우기\t7. 장바구니에 항목 삭제하기");
-		System.out.println("4. 영수증 표시하기\t8. 종료");
-		System.out.println("************************************************");
+		boolean quit = false;
 		
-		System.out.println("메뉴 번호를 선택하세요: ");
-		int choice = input.nextint();
-		System.out.println("choice + "번을 선택했습니다.);
-	}
+		while(!quit) {
+			
+			System.out.println("************************************************");
+			System.out.println("\t" + greeting);
+			System.out.println("\t" + tagline);
+			System.out.println("************************************************");
+			
+			System.out.println("1. 고객 정보 확인하기 \t5. 바구니에 항목 추가하기");
+			System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니에 항목 수량 줄이기");
+			System.out.println("3. 장바구니 비우기\t7. 장바구니에 항목 삭제하기");
+			System.out.println("4. 영수증 표시하기\t8. 종료");
+			System.out.println("************************************************");
+			
+			System.out.println("메뉴 번호를 선택하세요: ");
+			int choice = input.nextInt();
+			
+			if(choice < 1 || choice > 8) {
+				System.out.println("1부터 8까지의 숫자를 입력하시오");
+			} else {
+				switch(choice) {
+				case 1:
+					System.out.println("현재 고객 정보: ");
+					System.out.println("이름 " + name + "연락처" + phone);
+					break;
+				case 2:
+					System.out.println("장바구니 상품 목록 보기");
+					break;
+				case 3:
+					System.out.println("장바구니 비우기");
+					break;
+				case 4:
+					System.out.println("영수증 표시하기");
+					break;
+				case 5:
+					System.out.println("바구니에 항목 추가하기: ");
+					break;
+				case 6:
+					System.out.println("장바구니에 항목 수량 줄이기: ");
+					break;
+				case 7:
+					System.out.println("장바구니에 항목 삭제하기: ");
+					break;
+				case 8:
+					System.out.println("종료");
+					quit = true;
+					break;
+					
+				} // switch 끝
+			} // else 끝
+		} // while 끝
+	} //main 끝
 
-}
+} //welcome 끝
