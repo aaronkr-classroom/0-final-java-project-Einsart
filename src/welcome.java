62283447bf3538a1eb657eb763ff366c71edca16
Einswart <162276768+Einsart@users.noreply.github.com>
2024-03-26 17:15:44 +0900

    Finish

--- src/welcome.java
+++ src/welcome.java
@@ -61,7 +61,7 @@ public class welcome {
 					menucartbill();
 					break;
 				case 5:
-					menucartadditem();
+					menucartadditem(mBook);
 					break;
 				case 6:
 					menucartremoveitemcount();
@@ -121,8 +121,47 @@ public class welcome {
 
 	
 	}
-	public static void menucartadditem() {
-		System.out.println("5. 바구니에 항목 추가하기: ");
+	public static void menucartadditem(String[][] book) {
+		//System.out.println("5. 바구니에 항목 추가하기: ");
+		
+		Booklist(book);
+		for (int i = 0; i < NUM_BOOK; i++) {
+			for (int j = 0; j< NUM_ITEM; j++)
+				System.out.print(book[i][j] + "|");
+			System.out.println("");
+			
+		}
+		
+		boolean quit = false;
+		
+		while (!quit) {
+			System.out.print("장바구니에 추가할 도서의 ID를 입력하세요: ");
+			
+			Scanner input = new Scanner(System.in);
+			String str = input.nextLine();
+			
+			boolean flag = false;
+			int numId = -1;
+			
+			for (int i = 0; i< NUM_BOOK; i++) {
+				if (str.equals(book[i][0])) {
+					numId = i;
+					flag = true;
+					break;
+				}
+			}
+			
+			if (flag) {
+				System.out.println("장바구니에 추가하시겠습니까? Y | N");
+				str = input.nextLine();
+				
+				if(str.toUpperCase().equals("Y")) {
+					System.out.println(book[numId][0]+ "도서가 장바구니에 추가되었습니다");
+				}
+				quit = true;
+			} else
+				System.out.println("다시 입력해 주세요");
+		}
 	
 	}
 	public static void menucartremoveitemcount() {
@@ -142,31 +181,31 @@ public class welcome {
 	
 	}
 	
-	public static void booklist(String[][] book) {
+	public static void Booklist(String[][] book) {
 		
-		book[0][0] ="IDSA1234";
-		book[0][1] ="쉽게 배우는 웹프로그래밍";
-		book[0][2] ="2700";
+		book[0][0] ="ISBN1234";
+		book[0][1] ="쉽게 배우는 JSP 웹 프로그래밍";
+		book[0][2] ="27000";
 		book[0][3] ="송미영";
-		book[0][4] ="단꼐별로 쇼핑몰을";
+		book[0][4] ="단꼐별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍";
 		book[0][5] ="IT전문서";
 		book[0][6] ="2018/10/08";
 		
-		book[1][0] ="SOFQF1234";
-		book[1][1] ="쉽게 배우는 웹";
-		book[1][2] ="3700";
+		book[1][0] ="ISBN1235";
+		book[1][1] ="안드로이드 프로그래밍";
+		book[1][2] ="33000";
 		book[1][3] ="우재남";
-		book[1][4] ="단꼐별로 멘토링";
+		book[1][4] ="실습 단계별 명쾌한 멘토링!";
 		book[1][5] ="IT전문서";
 		book[1][6] ="2022/01/22";
 		
-		book[2][0] ="IDSA1235";
+		book[2][0] ="ISBN1236";
 		book[2][1] ="스크래치";
-		book[2][2] ="2200";
+		book[2][2] ="22000";
 		book[2][3] ="고광일";
-		book[2][4] ="단꼐별로 컴퓨터";
+		book[2][4] ="컴퓨터 사고력을 키우는 블록 코딩";
 		book[2][5] ="컴퓨터입문";
-		book[2][6] ="2019/10/08";
+		book[2][6] ="2019/06/10";
 		
 		
 	}
